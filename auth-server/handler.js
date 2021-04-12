03227fb7122f8f063ffb47512e716561a1454b08
Kumi <64310263+KumiGhardt@users.noreply.github.com>
2021-04-12 10:39:12 +0200

    This commit adds the README.md file

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -14,7 +14,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://kumighardt.github.io/meet/"],
+  redirect_uris: ["https://kumighardt.github.io/meet"],
   javascript_origins: ["https://kumighardt.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
@@ -38,10 +38,55 @@ const authUrl = oAuth2Client.generateAuthUrl({
 return {
   statusCode: 200,
   headers: {
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin": "*"
   },
   body: JSON.stringify({
     authUrl: authUrl,
   }),
 };
+};
+
+
+// getAccessToken serverless function now  to be deployed to AWS Lambda
+module.exports.getAccessToken = async (event) => {
+  //creact a new OAuthClient. // The values used to instantiate the OAuthClient are at the top of the file
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+    // Decode authorization code extracted from the URL query
+    const code = decodeURIComponent(`${event.pathParameters.code}`);
+    return newPromise((resolve, reject) => {
+        /**
+     *  Exchange authorization code for access token with a “callback” after the exchange,
+     *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
+     */
+
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(token);
+    });
+  })
+    .then((token) => {
+      // Respond with OAuth token
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify(token),
+      };
+    })
+    .catch((err) => {
+      // Handle error
+      console.error(err);
+      return {
+        statusCode: 500,
+        body: JSON.stringify(err),
+      };
+    });
 };

bedb427ade210efcce74278717e8ad914d42b0d7
Kumi <64310263+KumiGhardt@users.noreply.github.com>
2021-04-13 18:38:28 +0200

    coorecting the access token

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -38,7 +38,7 @@ const authUrl = oAuth2Client.generateAuthUrl({
 return {
   statusCode: 200,
   headers: {
-    "Access-Control-Allow-Origin": "*"
+    "Access-Control-Allow-Origin": "*",
   },
   body: JSON.stringify({
     authUrl: authUrl,
@@ -58,7 +58,7 @@ module.exports.getAccessToken = async (event) => {
 
     // Decode authorization code extracted from the URL query
     const code = decodeURIComponent(`${event.pathParameters.code}`);
-    return newPromise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         /**
      *  Exchange authorization code for access token with a “callback” after the exchange,
      *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
@@ -76,9 +76,58 @@ module.exports.getAccessToken = async (event) => {
       return {
         statusCode: 200,
         headers: {
-          "Access-Control-Allow-Origin": "*"
+          "Access-Control-Allow-Origin": "*",
         },
         body: JSON.stringify(token),
+      };
+    })
+    .catch((err) => {
+      // Handle error
+      console.error(err);
+      return {
+        statusCode: 500,
+        body: JSON.stringify(err),
+      };
+    });
+};
+
+
+/********************************************************************* */
+
+module.exports.getCalendarEvents = async (event) => {
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+    const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+    oAuth2Client.setCredentials({ access_token });
+
+    return new Promise((resolve, reject)=>{
+      calendar.events.list(
+        {
+          calendarId: calendar_id,
+          auth: oAuth2Client,
+          timeMin: new Date().toISOString(),
+          singleEvents: true,
+          orderBy: "startTime",
+        },
+        (error, response) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(response);
+          }
+        }
+      );
+    }).then((results) => {
+      // Respond with OAuth token
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({ events: results.data.items }),
       };
     })
     .catch((err) => {

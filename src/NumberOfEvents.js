4bb9845b1295360d66e9ab8755cf441887fda05e
Kumi <64310263+KumiGhardt@users.noreply.github.com>
2021-04-19 10:59:37 +0200

    modies number of events

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
+
+  state = {
+    query: '',
+  }
+
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({ query: value});
+  }
+
   render() {
     return (
       <div className="NumberOfEvents">
           <input
         type="number"
         min="1" max="32"
+        value={this.state.query}
+        onChange={this.handleInputChanged}
       />
       </div>
     );

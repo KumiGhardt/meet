cf3522d40c5f59ced24433eea4853f289bd72fae
Kumi <64310263+KumiGhardt@users.noreply.github.com>
2021-04-19 10:44:00 +0200

    this commit corrects the number of events test

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import NumberOfEvents from '../NumberOfEvents';
-import {mockData} from '../mock-data';
+import React from "react";
+import { shallow } from "enzyme";
+import NumberOfEvents from "../NumberOfEvents";
 
-describe('<NumberOfEvents /> component', () => {
-    let NumberOfEventsWrapper;
-    beforeAll(() => {
-        NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
-    })
+describe("<NumberOfEvents /> component", () => {
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+  });
 
-    test('render of number of events', () => {
-        expect(NumberOfEventsWrapper).toHaveLength(1);
+  test("render of number of events", () => {
+    expect(NumberOfEventsWrapper).toHaveLength(1);
+  });
+
+  test("change state when the number of events changes", () => {
+    NumberOfEventsWrapper.setState({
+      query: 1,
     });
-
-
-    test('change state when the number of events changes', () => {
-        NumberOfEventsWrapper.setState({
-            eventCount: 2
-        });
-        const eventObject = { target : { value: 1 }}; 
-        NumberOfEventsWrapper.find('.NumberOfEvents input').simulate('change', eventObject);
-        expect(NumberOfEventsWrapper.state('eventCount')).toBe(1);
-    })
-})
+    const eventObject = { target: { value: 3 } };
+    NumberOfEventsWrapper.find(".NumberOfEvents input").simulate(
+      "change",
+      eventObject
+    );
+    expect(NumberOfEventsWrapper.state("query")).toBe(3);
+  });
+});

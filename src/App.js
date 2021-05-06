53ea10754f9f94c2012ae8471a9e949405b0215a
Kumi <64310263+KumiGhardt@users.noreply.github.com>
2021-05-06 19:43:22 +0200

    this commit adds a responsive graph

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
 import { getEvents, extractLocations } from "./api";
 import { WarningAlert } from './Alert';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+} from 'recharts';
 
 
 class App extends Component {
@@ -59,11 +62,22 @@ class App extends Component {
     });
   }
 
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(' ').shift()
+      return {city, number};
+    })
+    return data;
+  };
+
   componentWillUnmount() {
     this.mounted = false;
   }
 
   render() {
+   // const { locations, numberOfEvents } = this.state;
     return (
       <div className="App">
         <WarningAlert text={this.state.warningText} />
@@ -75,7 +89,23 @@ class App extends Component {
         />
         <p>No. of events</p>
         <NumberOfEvents updateEvents={this.updateEvents} />
-        <p>Upcoming Events</p>
+        <h4>Upcoming Events</h4>
+
+        <ResponsiveContainer height={400} >
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis
+              allowDecimals={false}
+              type="number"
+              dataKey="number"
+              name="number of events"
+            />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+
         <EventList events={this.state.events} />
       </div>
     );

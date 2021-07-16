b92c374547f29ddb64ced47bb16d413679bf8a39
Kumi <64310263+KumiGhardt@users.noreply.github.com>
2021-07-16 17:14:19 +0200

    this commit updates the app

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,6 +1,6 @@
 //is where you’ll write the code for your functions
 const { google } = require("googleapis");
-const OAuth2 = google.auth.OAuth2;
+//const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
 
 //SCOPES allows you to set access levels
